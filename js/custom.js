55aed959fb51a9081889c183c102b55db9ce2ccc
Shauna <shauna_w@hotmail.com>
2015-10-29 17:20:40 +0800

    Responsive Web

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+var custom = {
+    init : function(){
+      custom.binding();
+    },
+    binding : function() {
+
+      
+    },
+
+};
+
+jQuery(document).ready(function($){
+    custom.init();
+});
